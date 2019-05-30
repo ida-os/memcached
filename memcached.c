183ddae5e28b9bdda9c78f757e633df4d9520624
esmailasyabi <esmail.asyabi@gmail.com>
2019-05-30 17:27:39 -0400

    clock

--- memcached.c
+++ memcached.c
@@ -5862,7 +5862,7 @@ static void drive_machine(conn *c) {
             
             int curr_time= current_time;
               //printf(" current timr %d ", curr_time - c->last_sampling_time  );
-            if((curr_time != c->last_sampling_time){ // showan //
+            if(curr_time != c->last_sampling_time){ // showan //
                 printf(" current timr %d ", curr_time - c->last_sampling_time  );
                c->rate= c->num_ops_over_last_window/(curr_time - c->last_sampling_time ); //showan
               

5da2618a88c67da7bd5b6d962cf834203793f07a
esmailasyabi <esmail.asyabi@gmail.com>
2019-05-31 13:49:50 -0400

    rate

--- memcached.c
+++ memcached.c
@@ -6542,7 +6542,7 @@ static struct event clockevent;
  * ensure their clocks are correct before starting memcached. */
 static void clock_handler(const int fd, const short which, void *arg) {
     //struct timeval t = {.tv_sec = 1, .tv_usec = 0}; // tshowan
-    struct timeval t = {.tv_sec = 0, .tv_usec = 20000};
+    struct timeval t = {.tv_sec = 0, .tv_usec = 1000};
     //
     static bool initialized = false;
 #if defined(HAVE_CLOCK_GETTIME) && defined(CLOCK_MONOTONIC)

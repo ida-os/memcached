688c46fea86ed10517c02f7daec7d6a41590ff5c
esmailasyabi <esmail.asyabi@gmail.com>
2019-06-11 17:09:09 -0400

    remove some errors

--- memcached.c
+++ memcached.c
@@ -6304,7 +6304,7 @@ the question is which connection- just randomly chooses one????*/
 //
     //}
     
-    
+    /*
     if(power_stat.victim_worker == c->thread->index)
     if (power_stat.load_balancing== true){
     
@@ -6323,7 +6323,7 @@ the question is which connection- just randomly chooses one????*/
       }
 
     }
-   
+   */
     
     return;
 }

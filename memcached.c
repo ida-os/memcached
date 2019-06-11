b42d64649dd2cbf33a5d9b08514382f9db74777d
esmailasyabi <esmail.asyabi@gmail.com>
2019-06-11 12:53:26 -0400

    remove some errors

--- memcached.c
+++ memcached.c
@@ -6299,10 +6299,10 @@ the question is which connection- just randomly chooses one????*/
 //    if(c->thread->am_i_a_dispatching)
   //  {
     //    denote_connection();
-
+//
     //}
     
-    if(power_stat.victim_worker == c->thread)
+    if(power_stat.victim_worker == c->thread->index)
     if (power_stat.load_balancing== true){
     
     c->is_guest = true;
